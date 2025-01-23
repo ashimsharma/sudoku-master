6a81248ea14cddf0916d4e1c8be3d7b3cc0584cb
ashimsharma <sharmaashim756@gmail.com>
2025-01-23 12:27:03 -0500

    Add Keyboard Numbers and Timer Logic

--- src/app/components/game-board/game-board.component.ts
+++ src/app/components/game-board/game-board.component.ts
@@ -10,23 +10,54 @@ import { NgFor, NgClass } from '@angular/common';
 })
 export class GameBoardComponent {
   Math = Math;
-  game: (Number | null)[];
-  solution: Number[];
-  selectedRow: Number = -1;
-  selectedColumn: Number = -1;
-  selectedBlock: Number = -1;
+  initialGameState: (number | null)[];
+  game: (number | null)[];
+  solution: number[];
+  selectedRow: number = -1;
+  selectedColumn: number = -1;
+  selectedBlock: number = -1;
+  selectedCell: number = -1;
 
   constructor() {
     this.game = [];
     this.solution = [];
+    this.initialGameState = [];
   }
   ngOnInit(): void {
     this.loadBoard();
+    window.addEventListener('keydown', this.handleKeyPress.bind(this))
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('keydown', this.handleKeyPress.bind(this));
+  }
+
+  handleKeyPress(event: KeyboardEvent): void {
+    const digit = Number(event.key);
+    if (this.selectedCell !== null && digit >= 1 && digit <= 9) {
+      this.addDigit(digit);
+    }
+  }
+
+  addDigit(digit: number): void {
+    if (this.selectedCell !== null && this.initialGameState[this.selectedCell] === null) {
+      this.game[this.selectedCell] = digit; 
+    }
   }
 
   loadBoard(): void {
     this.game = sudoku.makepuzzle();
     this.solution = sudoku.solvepuzzle(this.game);
+
+    this.game = this.game.map(cell => cell !== null ? cell + 1 : null);
+    this.solution = this.solution.map(cell => cell + 1);
+    
+    this.initialGameState = this.game;
+    console.log(this.solution);
+  }
+
+  isOnCorrectPosition(i: number, valueAdded: (number | null)): boolean {
+    return this.solution[i] !== valueAdded ? true : false;
   }
 
   selectRowAndColumn(i: number): void {
@@ -34,9 +65,9 @@ export class GameBoardComponent {
     const column = i % 9;
     const block = Math.floor(row / 3) * 3 + Math.floor(column / 3);
 
-    this.selectedRow = row;      // Store the selected row
-    this.selectedColumn = column; // Store the selected column
-    this.selectedBlock = block;   // Store the selected block
+    this.selectedRow = row;     
+    this.selectedColumn = column; 
+    this.selectedBlock = block;   
   }
 
   generateCellId(i: number): string {
@@ -51,9 +82,10 @@ export class GameBoardComponent {
     const column = i % 9;
     const block = Math.floor(row / 3) * 3 + Math.floor(column / 3);
 
-    this.selectedRow = row;       // Store the selected row
-    this.selectedColumn = column; // Store the selected column
-    this.selectedBlock = block;   // Store the selected block
+    this.selectedCell = i;
+    this.selectedRow = row;       
+    this.selectedColumn = column; 
+    this.selectedBlock = block;   
   }
 
   isRowSelected(i: number): boolean {
--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -9,8 +9,20 @@ import { Component } from '@angular/core';
 export class TimerComponent {
   secondsPassed: number = 0;
   minutesPassed: number = 0;
+  timerInterval: (ReturnType<typeof setInterval> | null) = null;
 
   get formattedTime(): string {
     return `${this.minutesPassed > 9 ? this.minutesPassed : `0${this.minutesPassed}`} : ${this.secondsPassed > 9 ? this.secondsPassed : `0${this.secondsPassed}`}`;
+  }
+
+  ngOnInit(): void{
+    this.timerInterval = setInterval(() => {
+      if(this.secondsPassed === 59){
+        this.minutesPassed += 1;
+        this.secondsPassed = 0;
+        return;
+      }
+      this.secondsPassed += 1;
+    }, 1000)
   }
 }
